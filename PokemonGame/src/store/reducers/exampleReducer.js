7e79b201bde8c9de35d9070f059ecd4f2bd93b5b
jaroonl <jaronslin@gmail.com>
2023-08-11 20:05:05 -0700

    react redux app

---
+++ PokemonGame/src/store/reducers/exampleReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  // Initial state properties
+};
+
+const exampleReducer = (state = initialState, action) => {
+  switch (action.type) {
+    // Define your action cases and state updates here
+    default:
+      return state;
+  }
+};
+
+export default exampleReducer;
